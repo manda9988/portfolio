4b6e652221b7d7c20e2fae7516978d6efaf51618
KAEL <vincent.achy@gmail.com>
2023-12-06 14:53:17 +0100

    commit

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,168 +1,81 @@
 // Personal Information --> #hero section
 export const name = "Vincent Achy";
 export const designation = "Développeur web | Intégrateur";
-// export const location = "Quahog, Rhode Island";
-// export const pronouns = "he/him";
-// export const website = "https://petergriffin.com";
 
 // About
 export const about = {
   paragraph1:
-    "Formé à l'école O'Clock et par mes expériences autodidactes, je me suis consacré au développement web depuis 3 ans. J'ai acquis de bonnes compétences en pratiquant des technologies variées comme: JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL.",
+    "Formé à l'école O'Clock et par mes expériences autodidactes, je me suis consacré au développement web depuis 3 ans. J'ai acquis de bonnes compétences en pratiquant des technologies variées comme: <span class='highlight'>JavaScript, SCSS, Svelte, React, Node.js, Express et PostgreSQL</span>.",
   paragraph2:
     "Mon expérience professionnelle m'a apporté des compétences essentielles en communication et travail d'équipe. Motivé par l'innovation et le développement de solutions pratiques, je suis prêt à m'engager dans des projets constructifs, apportant mon savoir-faire et ma capacité d'adaptation.",
 };
 
-// Work Experience --> #work section
-export const work = [
-  {
-    from: "2001",
-    to: "Present",
-    role: "Safety Inspector",
-    org: "Quahog Safety Department",
-    org_url: "https://quahog.gov/safety",
-    location: "Quahog, Rhode Island",
-    skills: "Safety Inspection, Risk Assessment, Emergency Response",
-  },
-];
-
 // Projects --> #project section
 export const projects = [
   {
-    title: "Project Griffin",
-    description:
-      "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-    url: "https://projectgriffin.com",
-    tech: "Safety Equipment, Training Programs, Community Outreach",
+    title: "g.Blog",
+    description: "Une application pour la création d'articles généralistes.",
+    url: "https://www.google.fr",
+    tech: "Svelte, Vite, SCSS, PostgreSQL, Node.js, Express.",
   },
   {
-    title: "Family Safety Campaign",
+    title: "Architect",
     description:
-      "An initiative to promote safety awareness among families in Quahog.",
-    url: "https://safefamilyquahog.com",
-    tech: "Workshops, Educational Material, Public Events",
+      "Un site présentant les projets éclectiques et innovants d'une jeune agence d'architecture.",
+    url: "https://www.google.fr",
+    tech: "React, Vite, SCSS, Node.js, TypeScript, Express.",
+  },
+];
+
+// Work Experience --> #work section
+export const work = [
+  {
+    from: "2022",
+    to: "2023",
+    role: "Formateur Intégrateur Web",
+    org: "Tremplin Numérique",
+    location: "Paris",
+    skills: "HTML, CSS, SCSS, JavaScript.",
   },
   {
-    title: "Road Safety Initiative",
-    description:
-      "A campaign to enhance road safety and reduce accidents in Quahog.",
-    url: "https://roadsafetyquahog.com",
-    tech: "Traffic Analysis, Signage Improvement, Public Awareness",
+    from: "2012",
+    to: "2022",
+    role: "Monteur Vidéo",
+    org: "Association ACC",
+    location: "Chantilly",
+    skills: "Final Cut Pro, iMovie.",
   },
 ];
 
 // Education --> #education section
 export const education = [
   {
-    from: "1995",
-    to: "1999",
-    institute: "Quahog High School",
-    course: "High School Diploma",
-    location: "Quahog, Rhode Island",
+    from: "2022",
+    to: "2023",
+    institute: "O'clock",
+    course: "Titre Professionnel Développeur Web et Web Mobile",
+    location: "Paris",
   },
   {
-    from: "1999",
-    to: "2003",
-    institute: "Quahog Community College",
-    course: "Bachelor of Science in Safety Management",
-    location: "Quahog, Rhode Island",
-  },
-  {
-    from: "2003",
-    to: "2005",
-    institute: "Safety University",
-    course: "Master of Science in Safety Engineering",
-    location: "Boston, Massachusetts",
+    from: "2010",
+    to: "2013",
+    institute: "Université Paris 8",
+    course: "Licence Cinéma et Audiovisuel",
+    location: "Paris",
   },
 ];
 
 // Contact --> #contact section
 export const contact = [
   {
-    source_name: "Email",
-    source: "peter.griffin@example.com",
+    source_name: "vincent.achy@gmail.com",
   },
   {
-    source_name: "Twitter",
-    source: "https://twitter.com/PeterGriffin",
+    source_name: "Github",
+    source: "https://github.com/manda9988",
   },
-  {
-    source_name: "LinkedIn",
-    source: "https://www.linkedin.com/in/peter-griffin",
-  },
-];
-
-// Certifications --> #certificate section
-
-export const certificates = [
-  {
-    issued: "2005",
-    title: "Certified Safety Inspector",
-    org: "National Safety Association",
-    url: "https://certi.com/url",
-    description: "Certification in safety inspection and risk assessment.",
-  },
-  {
-    issued: "2006",
-    title: "Emergency Response Training",
-    org: "Quahog Safety Department",
-    url: "https://certi.com/url",
-    description:
-      "Training in handling emergency situations and crisis management.",
-  },
-  {
-    issued: "2007",
-    title: "Safety Leadership Workshop",
-    org: "Safety Leaders International",
-    url: "https://certi.com/url",
-    description: "Leadership training in safety management and communication.",
-  },
-];
-
-// Blogs --> #blogs section
-export const blogs = [
-  {
-    date: "12-July, 2023",
-    url: "https://medium.com/",
-    title: "Adventures in Safety Inspection",
-    description:
-      "Join me in my humorous escapades as a safety inspector in Quahog.",
-    publisher: "Quahog Times",
-  },
-  {
-    date: "25-June, 2023",
-    url: "https://medium.com/",
-    title: "Top 10 Safety Tips for Families",
-    description:
-      "Learn the best safety practices for families to ensure a secure environment.",
-    publisher: "Safety Monthly",
-  },
-];
-
-// Achievements --> #achievement section
-export const achievements = [
-  {
-    title: "Safety Hero Award",
-    year: "2020",
-    decription:
-      "Recognized for outstanding contributions to improving safety standards in Quahog.",
-  },
-  {
-    title: "Safety Innovation Prize",
-    year: "2018",
-    decription:
-      "Awarded for implementing innovative safety measures that yielded significant improvements.",
-  },
-  {
-    title: "Community Safety Ambassador",
-    year: "2015",
-    decription:
-      "Honored for promoting safety awareness and education in the Quahog community.",
-  },
-  {
-    title: "Safety Campaign Excellence",
-    year: "2013",
-    decription:
-      "Acknowledged for leading a successful safety campaign that reached thousands of residents.",
-  },
+  // {
+  //   source_name: "LinkedIn",
+  //   source: "https://www.linkedin.com/in/peter-griffin",
+  // },
 ];
