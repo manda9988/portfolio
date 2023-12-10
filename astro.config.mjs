f19f58a8ae1c8d68b6804c88f0c0589db28b26f4
KAEL <vincent.achy@gmail.com>
2023-12-10 17:06:42 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
-  }),
+
+  site: "https://manda9988.github.io",
 });
